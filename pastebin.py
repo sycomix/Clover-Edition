39aadd4b47df1bfff931b10df93a9c8860dcc03a
cloveranon <cloveranon@nuke.africa>
2019-12-27 19:10:10 -0500

    remove useless TAGS line from pastebin

--- pastebin.py
+++ pastebin.py
@@ -3,7 +3,6 @@ import re
 import os
 from pathlib import Path
 
-# os.mkdir(Path('prompts', 'pastebin'))
 fnamesSoFar = {}
 
 
@@ -19,6 +18,8 @@ def filename(s):
 
 
 paste = request.urlopen("https://pastebin.com/raw/KD4yN2Gc").read().decode("utf-8")
+paste = re.sub(r'\nTAGS:.*\n', '\n', paste)
+#pipe is never used in paste so use it as a seperator
 paste = re.sub("=====+", "|", paste)
 paste = re.sub("\r", "", paste)
 paste = re.sub("\n\s*\n\s*", "\n\n", paste)
@@ -27,7 +28,7 @@ for sect in sections[2:][:-1]:
     category = re.search(r"\*\*\*([^\*]+)\*\*\*", sect).group(1)
     category = re.sub(".[pP]rompts?$", "", category)
     category = filename(category)
-    os.mkdir(Path("prompts", category))
+    Path("prompts", category).mkdir(exist_ok=True)
     print(category)
     for story in [x for x in filter(None, sect.split("\n\n"))][1:]:
         title = re.search(r"^\(([^\)]+)", story)
