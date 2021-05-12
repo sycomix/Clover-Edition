008df48fd3e14551359ef290e92dbc275b5f77f5
Skhmt <michael.shinn@outlook.com>
2021-05-11 19:44:25 -0700

    ghostbin update

--- aidungeon/pastebin.py
+++ aidungeon/pastebin.py
@@ -19,7 +19,7 @@ def filename(s):
 
 
 try:
-    paste = request.urlopen("https://ghostbin.co/paste/d4uh/raw").read().decode("utf-8")
+    paste = request.urlopen("https://ghostbin.co/paste/36vyxxa/raw").read().decode("utf-8")
 except error.HTTPError as e:
     if e.code == 404:
         output("Unable to find pastebin for scraping.", "error")
